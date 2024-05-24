4031b741057fa5afeb492b4d00e283deb5cc7ced
Yb <ackrmanlevi652@gmail.com>
2024-05-24 16:04:03 +0100

    main

--- main.cpp
+++ main.cpp
@@ -3,16 +3,15 @@
 #include "Filiere.h"
 #include "EmploiDuTemps.h"
 using namespace std;
-void printMenu() {
-    cout
-    << "1. Add Filiere\n"
-    << "2. Display Filieres\n"
-    << "3. Add Class\n"
-    << "4. Display Timetable\n"
-    << "5. Exit\n";
-        cout << "Enter Choice!:";
+void afficherMenu() {
+   cout
+        << "1. Ajouter une filière\n"
+        << "2. Afficher les filières\n"
+        << "3. Ajouter un cours\n"
+        << "4. Afficher l'emploi du temps\n"
+        << "5. Quitter\n";
+    cout << "Entrez votre choix : ";
 }
-
  int main() {
     Filiere filiere;
     EmploiDuTemps timetable;
@@ -20,7 +19,7 @@ void printMenu() {
     int choice = 0;
   while (choice != 5){
           cout << string(106, '-') << endl;
-          printMenu();
+          afficherMenu();
           cin >> choice;
           switch (choice)
           {
