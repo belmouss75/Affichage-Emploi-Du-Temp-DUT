ebc874a52310e98e5bca082af4eaa3bb93e24726
Yb <ackrmanlevi652@gmail.com>
2024-05-24 16:02:27 +0100

    main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
+#include <string>
+#include "Filiere.h"
+#include "EmploiDuTemps.h"
+using namespace std;
+void printMenu() {
+    cout
+    << "1. Add Filiere\n"
+    << "2. Display Filieres\n"
+    << "3. Add Class\n"
+    << "4. Display Timetable\n"
+    << "5. Exit\n";
+        cout << "Enter Choice!:";
+}
 
  int main() {
+    Filiere filiere;
+    EmploiDuTemps timetable;
+    string prof,day,start_time,end_time,subject,salle,Filierename;
     int choice = 0;
-    switch (choice)
-    {
-    case 1:
-      break;
+  while (choice != 5){
+          cout << string(106, '-') << endl;
+          printMenu();
+          cin >> choice;
+          switch (choice)
+          {
+              case 1:
+                  cout << "Enter Filiere Name: ";
+                  cin >> Filierename;
+                  filiere.ajouterFiliere(Filierename);
+                  break;
+             
+          }
 
-
-    case 2:
-        int a;
-    
-    default:
-      break;
-    }
+  }
 
 
     return 0;
